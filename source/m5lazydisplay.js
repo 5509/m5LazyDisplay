f487eed55ff6ee2e2c9af6942f12d67f9eb810d7
5509 <norimania@gmail.com>
2011-03-06 10:53:00 +0900

    rewrite var names

--- source/m5lazydisplay.js
+++ source/m5lazydisplay.js
@@ -6,35 +6,37 @@
  * @license      The MIT License
  * @link         https://github.com/5509/m5lazydisplay
  *
+ * @Modified     2011-03-05 10:49
+ *
  */
 ;(function($) {
 
 	$.fn.m5LazyDisplay = function(options) {
 		var _this = this,
-			_thisLen = _this.length,
-			_scrollPos = document.body.scrollTop || document.documentElement.scrollTop,
-			_clientHeight = document.body.clientHeight || document.documentElement.clientHeight,
-			_line = _clientHeight,
-			_c = $.extend({
+			thisLen = _this.length,
+			scrollPos = document.body.scrollTop || document.documentElement.scrollTop,
+			clientHeight = document.body.clientHeight || document.documentElement.clientHeight,
+			line = clientHeight,
+			c = $.extend({
 				posFix: 0,
 				duration: 400
 			}, options),
-			_imgObjs = [],
-			_currentImg = 0;
+			imgObjs = [],
+			currentImg = 0;
 			
 		for ( var i=0; i<_this.length; i++ ) {
-			_imgObjs[i] = {};
-			_imgObjs[i].y = $(_this[i]).offset().top;
-			_imgObjs[i].obj = $(_this[i]);
+			imgObjs[i] = {};
+			imgObjs[i].y = $(_this[i]).offset().top;
+			imgObjs[i].obj = $(_this[i]);
 			
-			if ( (_scrollPos + _clientHeight) < _imgObjs[i].y ) {
-				_imgObjs[i].obj.css("opacity", 0);
+			if ( (scrollPos + clientHeight) < imgObjs[i].y ) {
+				imgObjs[i].obj.css("opacity", 0);
 			} else {
-				_currentImg++;
+				currentImg++;
 			}
 		}
 		
-		_imgObjs.sort(
+		imgObjs.sort(
 			function(a, b) {
 				return a.y - b.y;
 			}
@@ -42,18 +44,18 @@
 		
 		$(window)
 			.resize(function() {
-				_clientHeight = document.body.clientHeight || document.documentElement.clientHeight;
+				clientHeight = document.body.clientHeight || document.documentElement.clientHeight;
 			})
 			.bind("scroll.LazyDisplayScroll", function() {
-				if ( _currentImg === _thisLen ) {
+				if ( currentImg === thisLen ) {
 					$(window).unbind("scroll.LazyDisplayScroll");
 					return false;
 				}
-				_scrollPos = document.body.scrollTop || document.documentElement.scrollTop;
-				while ( _imgObjs[_currentImg].y <= (_scrollPos + _c.posFix + _clientHeight) ) {
-					_imgObjs[_currentImg].obj.fadeTo(_c.duration, 1);
-					_currentImg++;
-					if ( _currentImg === _thisLen ) {
+				scrollPos = document.body.scrollTop || document.documentElement.scrollTop;
+				while ( imgObjs[currentImg].y <= (scrollPos + c.posFix + clientHeight) ) {
+					imgObjs[currentImg].obj.fadeTo(c.duration, 1);
+					currentImg++;
+					if ( currentImg === thisLen ) {
 						$(window).unbind("scroll.LazyDislayscroll");
 						break;
 					}
